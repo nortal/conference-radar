cbfda9dd473756cc260f7f85d1a55dafa1660057
Sander Hutsi <sander.hytsi@nortal.com>
2019-08-29 16:39:41 +0300

    Add submit page error messages

--- client/views/submit-view.js
+++ client/views/submit-view.js
@@ -53,6 +53,7 @@ function getRandomSection() {
 Template.submit.onCreated(function() {
     this.matches = new ReactiveVar([]);
     this.selectWidth = new ReactiveVar();
+    this.invalidInput = new ReactiveVar();
 });
 
 Template.submit.helpers({
@@ -68,7 +69,12 @@ Template.submit.helpers({
     matches: () => {
         return Template.instance().matches.get();
     },
-    getSelectWidth: () => { return Template.instance().selectWidth.get(); }
+    getSelectWidth: () => {
+        return Template.instance().selectWidth.get();
+    },
+    getInvalidInput: function () {
+        return Template.instance().invalidInput.get();
+    }
 });
 
 Template.submit.events({
@@ -78,7 +84,7 @@ Template.submit.events({
 
         var keywordName = template.$("#keywordText").val();
         var chosenStage = template.$("#stageDropdown").val();
-        var chosenSection = template.$("#sectionDropdown").val();
+        var chosenSection = template.$("#sectionText").val();
         var email = Session.get("email");
 
         // test data generation, uncomment to spam test data into database
@@ -87,83 +93,95 @@ Template.submit.events({
             //var chosenStage = getRandomStage();
             //var chosenSection = getRandomSection();
 
-            if (keywordName && keywordName.length && chosenStage != null && chosenSection != null) {
-                var keyword; // = Keywords.find({ keyword: newKeyword, stage: chosenStage, section: chosenSection }).fetch();
-                var allKeywords = Keywords.find().fetch();
 
-                // case insensitive search
-                for (i = 0; i < allKeywords.length; ++i) {
-                    if (allKeywords[i].keyword.toLowerCase() === keywordName.toLowerCase() &&
-                        allKeywords[i].stage === chosenStage &&
-                        allKeywords[i].section === chosenSection) {
-                        keyword = allKeywords[i];
-                        break;
-                    }
+            if (!keywordName || !keywordName.length || !chosenSection) {
+                template.invalidInput.set("No quadrant selected");
+                return;
+            }
+
+            if (!chosenStage) {
+                template.invalidInput.set("No stage selected");
+                return;
+            }
+
+            template.invalidInput.set();
+
+            var keyword; // = Keywords.find({ keyword: newKeyword, stage: chosenStage, section: chosenSection }).fetch();
+            var allKeywords = Keywords.find().fetch();
+
+            // case insensitive search
+            for (i = 0; i < allKeywords.length; ++i) {
+                if (allKeywords[i].keyword.toLowerCase() === keywordName.toLowerCase() &&
+                    allKeywords[i].stage === chosenStage &&
+                    allKeywords[i].section === chosenSection) {
+                    keyword = allKeywords[i];
+                    break;
                 }
+            }
 
-                if (keyword && keyword.emails && keyword.emails.indexOf(email) >= 0) {
-                    template.$('#keywordText').val("");
-                    template.$("#stageDropdown").val("0");
-                    template.$("#sectionDropdown").val("0");
-                    document.getElementById('alreadyVotedContainer').style.opacity = '1';
-                    window.setTimeout(() => {
-                        document.getElementById('alreadyVotedContainer').style.opacity = '0'
-                    }, 4000);
-                    return;
-                }
-
-                if (keyword) {
-                    var voteString = 'votes';
-                    var action = {};
-                    action[voteString] = 1;
-                    var addEmail = {emails: email};
-                    Keywords.update(
-                        { _id: keyword._id },
-                        {
-                            $addToSet: addEmail,
-                            $inc: action
-                        });
-                } else {
-                    var newKeyword = {
-                        keyword: keywordName,
-                        stage: chosenStage,
-                        section: chosenSection,
-                        emails: [email],
-                        votes: 1,
-                    };
-                    Keywords.insert(newKeyword);
-                }
-
-                var oldVote = allKeywords.find((kw) => {
-                    return kw.emails.indexOf(email) >= 0 &&
-                        kw.keyword === keywordName &&
-                        kw.section === chosenSection &&
-                        kw.stage !== chosenStage;
-                });
-
-                if (oldVote) {
-                    Keywords.update(
-                        { _id: oldVote._id },
-                        {
-                            $pull: {emails: email},
-                            $inc: {votes: -1}
-                        });
-                }
-
-                // Clear form
+            if (keyword && keyword.emails && keyword.emails.indexOf(email) >= 0) {
                 template.$('#keywordText').val("");
                 template.$("#stageDropdown").val("0");
-                template.$("#sectionDropdown").val("0");
-
-                document.getElementById('faderContainer').style.opacity = '1';
-
-                function fadeout() {
-                    document.getElementById('faderContainer').style.opacity = '0';
-                }
-
-                window.setTimeout(fadeout, 4000);
-
+                template.$("#sectionText").val("0");
+                document.getElementById('alreadyVotedContainer').style.opacity = '1';
+                window.setTimeout(() => {
+                    document.getElementById('alreadyVotedContainer').style.opacity = '0'
+                }, 4000);
+                return;
             }
+
+            if (keyword) {
+                var voteString = 'votes';
+                var action = {};
+                action[voteString] = 1;
+                var addEmail = {emails: email};
+                Keywords.update(
+                    { _id: keyword._id },
+                    {
+                        $addToSet: addEmail,
+                        $inc: action
+                    });
+            } else {
+                var newKeyword = {
+                    keyword: keywordName,
+                    stage: chosenStage,
+                    section: chosenSection,
+                    emails: [email],
+                    votes: 1,
+                };
+                Keywords.insert(newKeyword);
+            }
+
+            var oldVote = allKeywords.find((kw) => {
+                return kw.emails.indexOf(email) >= 0 &&
+                    kw.keyword === keywordName &&
+                    kw.section === chosenSection &&
+                    kw.stage !== chosenStage;
+            });
+
+            if (oldVote) {
+                Keywords.update(
+                    { _id: oldVote._id },
+                    {
+                        $pull: {emails: email},
+                        $inc: {votes: -1}
+                    });
+            }
+
+            // Clear form
+            template.$('#keywordText').val("");
+            template.$("#stageDropdown").val("0");
+            template.$("#sectionText").val("0");
+
+            document.getElementById('faderContainer').style.opacity = '1';
+
+            function fadeout() {
+                document.getElementById('faderContainer').style.opacity = '0';
+            }
+
+            window.setTimeout(fadeout, 4000);
+
+
         //}
     },
 
@@ -190,8 +208,9 @@ Template.submit.events({
 
         const data = $(event.target).data();
         template.matches.set([]);
+        template.invalidInput.set();
         template.$("#keywordText").val(data.name);
-        template.$("#sectionDropdown").val(data.section);
+        template.$("#sectionText").val(data.section);
     },
 
     'click #finishButton'() {
