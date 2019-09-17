89bd1c7d4a08cca112b4fe494123b9611553cd2e
Eduard Bortkevitš <eduard.bortkevits@nortal.com>
2019-09-17 17:54:39 +0300

    Create script for deploying to production.

---
+++ docker-deploy-prod.sh
@@ -0,0 +1,48 @@
+if [ -z "$1" ]
+then
+  echo "No remote host provided!"
+  exit 1
+fi
+
+remote=$1
+
+echo "==========================================="
+echo "REMOVE OLD ARCHIVE AND DOCKER IMAGE"
+echo "==========================================="
+rm conferenceRadar.tar
+docker rm -vf conferenceRadar
+docker rmi -f nortal/conference-radar
+
+echo "==========================================="
+echo "CREATE DOCKER IMAGE"
+echo "==========================================="
+docker build \
+  --build-arg NODE_VERSION=8.9.0 \
+  -t nortal/conference-radar:latest .
+
+echo "==========================================="
+echo "CREATE NEW DOCKER IMAGE ARCHIVE"
+echo "==========================================="
+docker save nortal/conference-radar:latest > conferenceRadar.tar
+
+echo "==========================================="
+echo "COPY ARCHIVE AND SETTINGS TO REMOTE HOST"
+echo "==========================================="
+scp -r conferenceRadar.tar settings-development.json ${remote}:/tmp/
+
+echo "==========================================="
+echo "RUN DOCKER IMAGE ON REMOTE HOST"
+echo "==========================================="
+ssh ${remote} << EOF
+  cd /tmp
+  docker rm -vf conferenceRadar
+  docker rmi -f nortal/conference-radar
+  docker load < /tmp/conferenceRadar.tar
+  docker run -d \
+    -e ROOT_URL=https://techradar.nortal.com \
+    -e MONGO_URL=mongodb://localhost:27017/meteor \
+    -e METEOR_SETTINGS="$(cat settings-development.json)" \
+    --name conferenceRadar \
+    --network="host" \
+    nortal/conference-radar:latest
+EOF
