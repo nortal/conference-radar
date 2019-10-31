954ce22316746c4d86b512df7d3e2cca0734861c
Sander Hutsi <sander.hytsi@nortal.com>
2019-10-31 12:45:45 +0200

    Update docker deployment scripts

--- docker-deploy-techradar.sh
+++ docker-deploy-techradar.sh
@@ -9,45 +9,46 @@ remote=$1
 echo "==========================================="
 echo "REMOVE OLD ARCHIVE AND DOCKER IMAGE"
 echo "==========================================="
-rm conferenceRadar.tar
-docker rm -vf conferenceRadar
-docker rmi -f nortal/conference-radar
+rm dist/techradar.tar
+docker rm -vf techradar
+docker rmi -f nortal/techradar
 
 echo "==========================================="
 echo "CREATE DOCKER IMAGE"
 echo "==========================================="
 docker build \
   --build-arg NODE_VERSION=8.9.0 \
-  -t nortal/conference-radar:latest .
+  -t nortal/techradar:latest .
 
 echo "==========================================="
 echo "CREATE NEW DOCKER IMAGE ARCHIVE"
 echo "==========================================="
-docker save nortal/conference-radar:latest > conferenceRadar.tar
+mkdir -p dist
+docker save nortal/techradar:latest > dist/techradar.tar
 
 echo "==========================================="
 echo "COPY ARCHIVE AND SETTINGS TO REMOTE HOST"
 echo "==========================================="
-scp -r conferenceRadar.tar settings-development.json ${remote}:/tmp/
+scp -r dist/techradar.tar settings-techradar.json ${remote}:/tmp/
 
 echo "==========================================="
 echo "RUN DOCKER IMAGE ON REMOTE HOST"
 echo "==========================================="
 ssh ${remote} << EOF
   cd /tmp
-  docker rmi -f nortal/conference-radar
-  docker load < /tmp/conferenceRadar.tar
+  docker rmi -f nortal/techradar
+  docker load < /tmp/techradar.tar
 
   for i in {0..3}
   do
-    docker rm -vf conferenceRadar\$i
+    docker rm -vf techradar-\$i
     docker run -d \
       -e ROOT_URL=https://techradar.nortal.com \
-      -e MONGO_URL=mongodb://localhost:27017/meteor \
-      -e METEOR_SETTINGS="\$(cat settings-development.json)" \
+      -e MONGO_URL=mongodb://localhost:27017/techradar \
+      -e METEOR_SETTINGS="\$(cat settings-techradar.json)" \
       -e PORT=300\$i \
       --network="host" \
-      --name conferenceRadar\$i \
-      nortal/conference-radar:latest
+      --name techradar-\$i \
+      nortal/techradar:latest
   done
 EOF
